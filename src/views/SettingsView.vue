f8389448149a22bc7e89ad412cba43491612ec37
TheBlackbird14 <90150013+TheBlackbird14@users.noreply.github.com>
2023-12-17 02:48:58 +0100

    Added placeholder components for settings and timetable

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-
+import HomeworkHeader from '@/components/homeworkHeader.vue'
 </script>
 
 <template>
-  $END$
+  <homework-header title="Einstellungen" :button="false" />
+
+  <h2>kommt bald</h2>
 </template>
 
 <style scoped>
-
+* {
+  color: var(--text-primary-color);
+}
 </style>
