6594fea4d462d37fe837ae0a8b4d41abe18d0da6
TheBlackbird14 <90150013+TheBlackbird14@users.noreply.github.com>
2023-12-17 02:49:29 +0100

    formatting

--- src/components/foodCarouselItem.vue
+++ src/components/foodCarouselItem.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
 import type { foodScheduleEntry } from '@/scripts/types/food-schedule.interface'
-import { computed, reactive } from 'vue'
+import { computed } from 'vue'
 
 const props = defineProps({
   foodEntry: {
